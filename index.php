87234723b4e621a372bb3a9b5f7b5c0d5f540d4b
david <david@carboni.io>
2014-03-26 17:11:03 +0000

    Bootstrapped the home page.

--- index.php
+++ index.php
@@ -1,9 +1,91 @@
-<h1>BOF2 Simulator</h1>
-<h2><a href="server.php">server</a> - <a href="client.php?sourceURN=1234567890&pncId=10&capture=testcapture">client</a>
+<!DOCTYPE html>
+<html lang="en">
+      
 
-<h3>Tutorial resources</h3>
-<p>
-<a href="http://www.codeproject.com/Articles/140189/PHP-NuSOAP-Tutorial">http://www.codeproject.com/Articles/140189/PHP-NuSOAP-Tutorial</a>
-<p>
-<a href="http://www.sitepoint.com/web-services-with-php-and-soap-1/">http://www.sitepoint.com/web-services-with-php-and-soap-1/</a>
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Personal cryptography">
+<meta name="author" content="David Carboni">
+
+<title>BOF2 SOAP server simulator</title>
+
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.css" rel="stylesheet">
+
+<style type="text/css">
+h3 a:hover {
+text-decoration:none;
+}
+</style>
+
+</head>
+
+  <body>
+  
+  <!-- Hello there -->
+
+    <div class="container">
+
+
+
+<div class="masthead">
+	<ul class="nav nav-pills pull-right">
+		<li class="active"><a href="/">Home</a></li>
+		<li><a href="https://github.com/davidcarboni/bof2">Github</a></li>
+		<li><a href="https://github.com/davidcarboni">Contact</a></li>
+	</ul>
+	<h3 class="text-muted" style="text-decoration:none"><a href="/">BOF2 Simulator</a></h3>
+</div>
+
+<hr />
+
+
+		<!-- Jumbotron -->
+		<div class="jumbotron">
+		
+			<h1>BOF2 Simulator</h1>
+		
+			<p>This app simulates a BOF2 SOAP server.
+			 </p>
+
+			<a href="server.php" class="btn btn-large btn-primary">Server details</a> 
+			<a href="server.php?wsdl" class="btn btn-large btn-primary">WSDL</a> 
+			<a href="client.php?sourceURN=1234567890&pncId=10&capture=testcapture" class="btn btn-large btn-primary">Client test..</a> 
+			
+		</div>
+
+
+      <div class="body-content">
+
+			<!-- Folder contents -->
+	        <div class="row">
+				<div class="col-lg-12">
+					<h2>Tutorial resources</h2>
+					<ul>
+					<li><a href="http://www.codeproject.com/Articles/140189/PHP-NuSOAP-Tutorial">http://www.codeproject.com/Articles/140189/PHP-NuSOAP-Tutorial</a>
+					<li><a href="http://www.sitepoint.com/web-services-with-php-and-soap-1/">http://www.sitepoint.com/web-services-with-php-and-soap-1/</a>
+					</ul>
+
+		        	</ul>
+				</div>
+	        </div>
+
+      </div> <!-- /.body-content -->
+      
+
+<hr />
+
+<!-- Site footer -->
+<div class="footer">
+	<p>&copy; 2014</p>
+</div>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
+
+
 
