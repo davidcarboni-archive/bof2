8009fce4d700ffa2d37fdefbbd61657d1ce0a4d3
david <david@carboni.io>
2014-03-26 16:56:48 +0000

    Added comments.

--- client.php
+++ client.php
@@ -1,12 +1,18 @@
 <?php
 require_once "lib/nusoap.php";
+
+
+// SOAP client - for local testing or on Heroku:
+
 //$client = new nusoap_client("http://localhost/anpr/server.php");
 $client = new nusoap_client("http://bof2.herokuapp.com/server.php");
- 
 $error = $client->getError();
 if ($error) {
     echo "<h2>Constructor error</h2><pre>" . $error . "</pre>";
 }
+
+
+// Call the SOAP server:
 
 // http://stackoverflow.com/questions/6459171/how-to-set-default-value-to-a-string-in-php-if-another-string-is-empty
 // For a 64-bit system, intval should work, but worth checking to be sure:
@@ -17,6 +23,9 @@ $capture = $_GET["capture"] ?: "001010011001001010101010100101";
  
 $result = $client->call("alertForCapture", array("sourceURN" => $sourceURN, "pncId" => $pncId, "capture" => $capture));
  
+
+// Display the results:
+
 if ($client->fault) {
     echo "<h2>Fault</h2><pre>";
     print_r($result);
--- server.php
+++ server.php
@@ -1,16 +1,17 @@
 <?php
 require_once "lib/nusoap.php";
- 
+
+// This is the web service action:
 function alertForCapture($sourceURN, $pncId, $capture) {
         return join(",", array(
             "Source URN: ".$sourceURN,
             "PNC ID: ".$pncId,
             "Capture: ".$capture));
 }
- 
+
+// This sets up a SOAP server with a WSDL:
 $server = new soap_server();
 $server->configureWSDL("AlertWebService", "bof:alert");
- 
 $server->register("alertForCapture",
     array("sourceURN" => "xsd:long", "pncId" => "xsd:integer", "capture" => "xsd:string"),
     array("return" => "xsd:string"),
@@ -19,7 +20,8 @@ $server->register("alertForCapture",
     "rpc",
     "encoded",
     "Receive an alert for a capture match from a remote BOF machine");
- 
+
+// Process the request:
 $server->service($HTTP_RAW_POST_DATA);
 
 ?>
